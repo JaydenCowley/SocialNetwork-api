1d50508e5e939144315c38815222696a87333abb
Jayden Cowley <jcowley16@gmail.com>
2022-08-19 13:22:50 -0600

    adding Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,35 @@
-const { Schema, model, type } = require('mongoose');
+const { Schema, model, type } = require("mongoose");
+// importing date-fns to format dates
+const { format } = require('date-fns')
+//Reaction sub document for reactions
+const Reaction = new Schema(
+    {
+        reactionId:
+    }
+)
 
+// model for thoughts
 const Thought = new Schema({
-    thoughtText
-})
+  thoughtText: {
+    type: String,
+    required: "Must enter thought text",
+    length: 1 | 280,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtTime) => format(createdAtTime, 'PPpp')
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema],
+
+});
+Thought.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+module.exports = Thought;
